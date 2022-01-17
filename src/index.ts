7f22ae5ef01bc33f549ae3c04b3f6fa31f3ecd44
Roz <roz@rjmunhoz.me>
2022-01-17 17:06:46 -0300

    fix: remove sentry

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
 import { config } from './app.config'
-import * as Sentry from '@sentry/node'
+// import * as Sentry from '@sentry/node'
 import server from './presentation/server'
-import { RewriteFrames } from '@sentry/integrations'
+// import { RewriteFrames } from '@sentry/integrations'
 
-if (config.sentry.dsn) Sentry.init({
-  dsn: config.sentry.dsn,
-  integrations: [
-    new RewriteFrames({
-      root: __dirname || process.cwd()
-    })
-  ]
+// if (config.sentry.dsn) Sentry.init({
+//   dsn: config.sentry.dsn,
+//   integrations: [
+//     new RewriteFrames({
+//       root: __dirname || process.cwd()
+//     })
+//   ]
+// })
+
+server.start(config).catch((err) => {
+  console.error('===== Fatal Error =====')
+
+  const errStr = JSON.stringify(err)
+  const errMsg = errStr !== '{}' ? errStr : err
+
+  console.error(errMsg)
+  process.exit(1)
 })
-
-server.start(config)
-  .catch(err => {
-    console.error('===== Fatal Error =====')
-
-    const errStr = JSON.stringify(err)
-    const errMsg = errStr !== '{}' ? errStr : err
-
-    console.error(errMsg)
-    process.exit(1)
-  })
